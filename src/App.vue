b02a63f822e40ed70973f18d20a30edb3da59211
zhujiawie <798474927@qq.com>
2021-08-16 10:49:35 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <button @click="isShowList">优化前</button>
+    <button>优化后</button>
+    <list-dialog ref="listDialog"/>
+  </div>
+</template>
+
+<script>
+import ListDialog from './views/ListDialog.vue'
+export default {
+  name: 'App',
+  components: {
+    ListDialog,
+  },
+  methods: {
+    isShowList(){
+      this.$refs.listDialog.show = !this.$refs.listDialog.show 
+    }
+  }
+}
+</script>
+
---
+++ src/views/ListDialog.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-show="show" class="dialogBox">
+    <ul>
+      <li v-for="(item, index) in arrMsg" :key="index" class="listItem">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListDialog",
+  data() {
+    return {
+      show: false,
+      arrMsg: [],
+    };
+  },
+  mounted() {
+    for (let index = 0; index < 400000; index++) {
+      this.arrMsg.push(index);
+    }
+  },
+};
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+.dialogBox {
+  width: 500px;
+  height: 1000px;
+  background: #f5f6f8;
+  overflow-y: auto;
+}
+.listItem {
+  width: 100%;
+  height: 100px;
+  background: #fff;
+  border: 1px solid bisque;
+  text-align: center;
+  line-height: 100px;
+}
+</style>
